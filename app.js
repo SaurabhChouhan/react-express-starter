f34e15178b7b53d118c3249a191bb33747a23168
Saurabh Chouhan <schouhan@aripratech.com>
2019-05-07 18:51:31 +0530

    - Added babel in order to use es6 constructs like import

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import createError from 'http-errors'
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import mongoose from 'mongoose'
+import indexRouter from './routes/index'
+import usersRouter from './routes/users'
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -19,8 +18,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+mongoose.connect('mongodb://localhost/react-express-starter', {useNewUrlParser:true});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ const.js
@@ -0,0 +1,2 @@
+export const ROLE_APP_USER = "ROLE_APP_USER"
+export const ROLE_ADMIN = "ROLE_ADMIN"
