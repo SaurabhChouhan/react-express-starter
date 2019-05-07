aea0deb647965b992a55e13356361fe920cf651b
Saurabh Chouhan <schouhan@aripratech.com>
2019-05-07 18:52:45 +0530

    - Added register API that registers a user, an appropriate sald/hash would be generated from sent password

---
+++ models/userModel.js
@@ -0,0 +1,15 @@
+import mongoose, {Schema} from 'mongoose'
+import passportLocalMongoose from 'passport-local-mongoose'
+
+var userSchema = new Schema({
+    name: String,
+    email: String,
+    password: String,
+    roles: [String],
+    isDeleted: {type: Boolean, default: false}
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const UserModel = mongoose.model("User", userSchema)
+export default UserModel
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
+import express from 'express'
+import {ROLE_APP_USER, ROLE_ADMIN} from '../const'
+import User from '../models/userModel'
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/register', function(req, res) {
+  User.register(new User({ username : req.body.email, name:req.body.name, email:req.body.email, roles:[ROLE_APP_USER] }), req.body.password, function(err, user) {
+      if (err) {
+        return res.json({success:false, error:err})
+      }
+      return res.json(user)
+  });
 });
 
 module.exports = router;
